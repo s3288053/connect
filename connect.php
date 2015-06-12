f62df183284a629de70326bda113d777755faa59
s3288053 <s3288053@student.rmit.edu.au>
2015-06-12 12:19:15 +1000

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,15 +2,15 @@
 
 require_once(‘db.php’);
 if(!$dbconn = mysql_connect(DB_HOST, DB_USER, DB_PW)){
-echo “could not connect to mysql on ‘. DB_HOST . ‘\n’;
+echo “could not connect to mysql on ". DB_HOST . '\n';
 exit;
 }
-echo ‘connected to mysql :)<br/>’;
+echo 'connected to mysql :)<br/>';
 if(!mysql_select_db(DB_NAME, $dbconn)){
-echo “could not connect to user database ‘ . DB_NAME. ‘\n’;
-echo mysql_error() . ‘\n’;
+echo "could not connect to user database " . DB_NAME. '\n';
+echo mysql_error() . '\n';
 exit;
 }
-echo “connected to database :) :) . DB_NAME;
+echo "connected to database :) :)" . DB_NAME;
 
 ?>
