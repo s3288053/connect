9a0b0ae2e5690dbe0856d7c6fe5a8b3a167923ce
Ubuntu <ubuntu@ip-172-31-2-225.ap-southeast-2.compute.internal>
2015-06-12 10:24:00 +0000

    this is an update for what was originally ment to happen

--- connect.php
+++ connect.php
@@ -1,8 +1,21 @@
 hmmm..
 <?php
 
-require_once(‘db.php’);
+require_once('db.php');
 if(!$dbconn = mysql_connect(DB_HOST, DB_USER, DB_PW)){
+<<<<<<< HEAD
+echo "could not connect to mysql on " . DB_HOST . "\n";
+echo mysql_error() . "/n";
+exit;
+}
+echo "<h1>Awesome!</h1> <h2>We have connected to mysql </h2> <h3>Massive happy face :)</h3><br/>";
+if(!mysql_select_db(DB_NAME, $dbconn)){
+echo "could not connect to user database" . DB_NAME. "\n";
+echo mysql_error() . "/n";
+exit;
+}
+echo "<h1>connected to database :) :) " . DB_NAME . "</h1>";
+=======
 echo “could not connect to mysql on ". DB_HOST . '\n';
 exit;
 }
@@ -13,5 +26,6 @@ echo mysql_error() . '\n';
 exit;
 }
 echo "connected to database :) :)" . DB_NAME;
+>>>>>>> 6167a797ccbcd3325f139daad4ea8156c8319b89
 
 ?>
